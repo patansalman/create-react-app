ba76b52eecd12f09532288e303563b0761a19f1c
patansalman <42342329+patansalman@users.noreply.github.com>
2020-05-05 13:22:38 +0530

    Add files via upload

---
+++ precache-manifest.8fd9c01503b2aa1c6b9d57de35a527a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1583f1b54b06a4ea2ac1a78f9cdf50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc8fd17b3b9dd310ba80",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f0f6b96b5c9935c892a6",
+    "url": "/static/js/2.b13ae2ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b13ae2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc8fd17b3b9dd310ba80",
+    "url": "/static/js/main.64685760.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
